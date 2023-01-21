1f8cdc0764f61d833f19c82198abfe80adf71ced
brentgr@microsoft.com <brent.groom@live.com>
2023-01-20 23:16:55 -0700

    revisions for better comments and log levels

--- commands-cloudbuild.sh
+++ commands-cloudbuild.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
+# to do list
+# change the font color to yellow for all verbose debug statements
+# Change the font to green for all information commands
+# specify the log analytics workspace name so it is consistent
+
 FILENAME="https://github.com/Azure-Samples/containerapps-albumapi-csharp/archive/refs/heads/main.zip"
 mkdir ./containerapps-albumapi-csharp
 cd ./containerapps-albumapi-csharp
 #curl -sS http://foo.bar/filename.zip > file.zip
 curl -SL $FILENAME > file.zip
-unzip -d . file.zip 
+unzip -d -o . file.zip 
 rm file.zip
 cd ./containerapps-albumapi-csharp-main/src
 pwd
+
+: << 'COMMENT'
+Ideas for log levels
+0 - Emergency (emerg)
+1 - Alerts (alert)
+2 - Critical (crit)
+3 - Errors (err)
+4 - Warnings (warn)
+5 - Notification (notice)
+6 - Information (info)
+7 - Debug (debug) 
+8 - Trace (trace) 
+COMMENT
+
+LOG_INFO=6
+LOG_DEBUG=7
+LOG_TRACE=8
+LOG_LEVEL=$LOG_DEBUG
+LOG_LEVEL=$LOG_INFO
 
 : << 'COMMENT'
 echo -e "\e[1;31m Bold Red \e[0;31m"
@@ -52,8 +76,7 @@ COMMENT
 #az provider register --namespace Microsoft.OperationalInsights
 
 #setup environment variables
-UNIQUE_ID="3"
-VERBOSEDEBUG=true
+UNIQUE_ID="4"
 RESOURCE_GROUP="rg-containerapp-$UNIQUE_ID"
 LOCATION="westus"
 ENVIRONMENT="env-album-containerapps"
@@ -72,15 +95,25 @@ ACR_NAME="crbrent$UNIQUE_ID"
 # https://stackoverflow.com/questions/47018863/parsing-and-storing-the-json-output-of-a-curl-command-in-bash
 #declare -a CIE_ACR
 
-###################################################
-# check for and create the resource group
-#
+##############################################
+##
+## Start section for Resource Group 
+##
 
-if $VERBOSEDEBUG
+if (($LOG_LEVEL>=$LOG_DEBUG))
 then
     echo "Running command to check for resource group:"
     echo "az group exists -n $RESOURCE_GROUP"
-    az group exists -n $RESOURCE_GROUP
+    #if ((a > 5)); then echo "a is more than 5"; fi
+
+    if (($LOG_LEVEL>=$LOG_TRACE))
+    then
+        az group exists -n $RESOURCE_GROUP
+    fi
+fi
+if (($LOG_LEVEL>=$LOG_INFO))
+then
+    echo "Checking for resource group:$RESOURCE_GROUP"
 fi
 SH_RG=$(az group exists -n $RESOURCE_GROUP)
 if [[ $SH_RG = *true* ]]
@@ -99,27 +132,49 @@ else
     fi
 fi
 
+##
+## End section for Resource Group 
+##
+##############################################
+
+
+##############################################
+##
+## Start section for Azure Container Registry
+##
+
 #retVal=42
 #return ${retVal} 2>/dev/null || exit "${retVal}"
 
 #CIE_ACR = $(('az acr show --name $ACR_NAME --resource-group $RESOURCE_GROUP'))
 # https://learn.microsoft.com/en-us/cli/azure/group?view=azure-cli-latest#az-group-exists
-#if $VERBOSEDEBUG
+#if (($LOG_LEVEL>=$LOG_DEBUG))
 #then
 #    echo "Running command:"
 #    echo "az acr show --name $ACR_NAME --resource-group $RESOURCE_GROUP"
 #    az acr show --name $ACR_NAME --resource-group $RESOURCE_GROUP
 #fi
-if $VERBOSEDEBUG
+if (($LOG_LEVEL>=$LOG_DEBUG))
 then
+    echo "Running command to check for azure container registry:"
+    echo "az acr show --name $ACR_NAME --resource-group $RESOURCE_GROUP"
     az acr show --name $ACR_NAME --resource-group $RESOURCE_GROUP
 fi
+
+echo "$(tput setaf 2)Checking for azure container registry $ACR_NAME $(tput setaf 7) "
 CIE_ACR=$(az acr show --name $ACR_NAME --resource-group $RESOURCE_GROUP)
 #echo ${CIE_ACR} # This doesn't render correctly- not sure why
 if [[ $CIE_ACR = *creationDate* ]]
 then
     echo "The Azure Container Registry already exists"
+    if (($LOG_LEVEL>=$LOG_DEBUG))
+    then
+        echo "Running command to get the container registry credentials"
+        echo "az acr credential show -n $ACR_NAME --query passwords[0].value"
+    fi
     CPASSWORD=$(az acr credential show -n $ACR_NAME --query passwords[0].value)
+    # Strip the quotes off the password
+    eval CPASSWORD=$CPASSWORD
 else
     echo "$(tput setaf 2) Create the Azure Container Registry"
 
@@ -137,21 +192,59 @@ else
         echo "$SH_ACRC"
         return 42
     fi
-    echo "$(tput setaf 2)Exiting the script. You need to update the script with the username and password from the Azure Container Registry. "
+    echo "$(tput setaf 2) Exiting the script. "
     CPASSWORD=$(az acr credential show -n $ACR_NAME --query passwords[0].value)
+    
+    # Strip the quotes off the password
+    eval CPASSWORD=$CPASSWORD
 
-    #return 42
 fi
 
-# Strip the quotes off the password
-eval CPASSWORD=$CPASSWORD
 
+##
+## End section for Azure Container Registry
+##
+##############################################
+
+
+##############################################
+##
+## Start section to perform a cloud build of the image and store it in the registry
+##
 # Build the container in Azure
 # To do - how to know when the container needs to be rebuilt. Perhaps a flag on the script?
-az acr build --registry $ACR_NAME --image $IMAGE_NAME --only-show-errors .
+if (($LOG_LEVEL>=$LOG_DEBUG))
+then
+    echo "Running command to perform a cloud build:"
+    echo "  az acr build --registry $ACR_NAME --image $IMAGE_NAME --only-show-errors ."
+fi
+echo "$(tput setaf 2)Performing cloud build of image:$IMAGE_NAME in registry:$ACR_NAME $(tput setaf 7) "
+SH_ACR_BUILD=$(az acr build --registry $ACR_NAME --image $IMAGE_NAME --only-show-errors .)
+if [[ $SH_ACR_BUILD = *successful* ]]
+then
+    echo "$(tput setaf 2)Finished successful build $(tput setaf 7) "
+else
+    ## to do check the result for errors and output the error messages
+    echo "Build failed"
+    return 42
+fi
+
+
+##
+## End section to perform a cloud build of the image and store it in the registry
+##
+##############################################
+
+## Debug/test exit 
+#return 42
+
+##############################################
+##
+## Start Container App Environment create
+##
 
 # https://learn.microsoft.com/en-us/cli/azure/containerapp/env?view=azure-cli-latest
-if $VERBOSEDEBUG
+if (($LOG_LEVEL>=$LOG_DEBUG))
 then
     az containerapp env show -n $ENVIRONMENT -g $RESOURCE_GROUP
 fi
@@ -169,9 +262,18 @@ else
       --resource-group $RESOURCE_GROUP \
       --location "$LOCATION"
 fi
+##
+## End Container App Environment create
+##
+##############################################
 
-# Check if the container app has already been created. If not, then create it. If yes, then update it
-if $VERBOSEDEBUG
+##############################################
+##
+## Start Container App create 
+##
+
+## Check if the container app has already been created. If not, then create it. If yes, then update it
+if (($LOG_LEVEL>=$LOG_DEBUG))
 then
     az containerapp show -n $CONTAINER_APP_NAME -g $RESOURCE_GROUP
 fi
@@ -199,10 +301,16 @@ else
     echo "az containerapp create --name $CONTAINER_APP_NAME --resource-group $RESOURCE_GROUP --environment $ENVIRONMENT --image $ACR_NAME.azurecr.io/$IMAGE_NAME --target-port 3500 --ingress 'external' --registry-server $ACR_NAME.azurecr.io --query properties.configuration.ingress.fqdn --registry-password $CPASSWORD --registry-username $CUSERNAME"
     az containerapp create --name $CONTAINER_APP_NAME --resource-group $RESOURCE_GROUP --environment $ENVIRONMENT --image $ACR_NAME.azurecr.io/$IMAGE_NAME --target-port 3500 --ingress 'external' --registry-server $ACR_NAME.azurecr.io --query properties.configuration.ingress.fqdn --registry-password $CPASSWORD --registry-username $CUSERNAME
 fi
+##
+## End Container App create 
+##
+##############################################
 
-#############################
+##############################################
+##
+## Start Verify the API returns a result you expect
+##
 
-# Verify the API returns a result you expect
 API_BASE_URL=$(az containerapp show --resource-group $RESOURCE_GROUP --name $CONTAINER_APP_NAME --query properties.configuration.ingress.fqdn -o tsv)
 API_BASE_URL="https://$API_BASE_URL/albums"
 CURL_RESULT=$(curl $API_BASE_URL)
@@ -214,7 +322,10 @@ else
     echo "Failed to access the API $API_BASE_URL"
 fi
 
-#############################
+##
+## End Verify the API returns a result you expect
+##
+##############################################
 
 
 ##docker login crbrent.azurecr.io
